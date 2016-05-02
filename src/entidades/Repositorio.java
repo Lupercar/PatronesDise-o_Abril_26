2b3e1e753b6422bb27445ca5733adcf72c469e90
JLGG <hotolete@gmail.com>
2016-05-02 22:23:27 +0200

    Creo una lista de tipo Producto en la clase Repositorio, y al constructor privado de Repositorio le añado 3 productos en esta lista.

--- src/entidades/Repositorio.java
+++ src/entidades/Repositorio.java
@@ -1,9 +1,20 @@
 package entidades;
 //    Aplico el patron Singleton para que Repositorio sea unico
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Repositorio {
     private static Repositorio instance = new Repositorio(); 
     
+//    Hago una lista de Producto
+    private List<Producto> productos = new ArrayList<>(); 
+    
     private Repositorio() {
+//      En el constructor añado 3 productos
+        productos.add(new Producto(1, "Producto 1")); 
+        productos.add(new Producto(2, "Producto 2")); 
+        productos.add(new Producto(3, "Producto 3")); 
     }
     
     static public Repositorio getInstance(){
