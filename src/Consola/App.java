2d5d437c7339c211b42bc1fbf1aa538f2385198a
JLGG <hotolete@gmail.com>
2016-05-02 21:10:53 +0200

    Instancio el mismo objeto de la clase mediante: Singleton s = Singleton.getInstance();

--- src/Consola/App.java
+++ src/Consola/App.java
@@ -1,8 +1,13 @@
 package Consola;
 
+import singleton.Singleton;
+
 public class App {
 
     public static void main(String[] args) {
-        
+//      con metodos estáticos no preciso una instancia 
+//      con el mismo nombre de la clase tengo acceso a los 
+//      metodos estaticos.
+        Singleton s = Singleton.getInstance(); //crea el objeto sino existe y lo devuelve
     }
 }
