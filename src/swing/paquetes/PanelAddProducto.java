0fedd9965ce3b5d89717cfaa582b158ff859ded7
JLGG <hotolete@gmail.com>
2016-05-03 00:42:42 +0200

    Añado un boton, lo denomino btnAdd

--- src/swing/paquetes/PanelAddProducto.java
+++ src/swing/paquetes/PanelAddProducto.java
@@ -32,10 +32,13 @@ public class PanelAddProducto extends javax.swing.JPanel {
         lblNombre = new javax.swing.JLabel();
         txtCodigo = new javax.swing.JTextField();
         txtNombre = new javax.swing.JTextField();
+        btnAdd = new javax.swing.JButton();
 
         lblCodigo.setText("Codigo:");
 
         lblNombre.setText("Nombre:");
+
+        btnAdd.setText("Add");
 
         javax.swing.GroupLayout MarcoLayout = new javax.swing.GroupLayout(Marco);
         Marco.setLayout(MarcoLayout);
@@ -51,9 +54,10 @@ public class PanelAddProducto extends javax.swing.JPanel {
                         .addComponent(lblNombre)
                         .addGap(14, 14, 14)))
                 .addGroup(MarcoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAdd)
                     .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(205, Short.MAX_VALUE))
+                .addContainerGap(21, Short.MAX_VALUE))
         );
         MarcoLayout.setVerticalGroup(
             MarcoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -66,7 +70,9 @@ public class PanelAddProducto extends javax.swing.JPanel {
                 .addGroup(MarcoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblNombre)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(199, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(btnAdd)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -90,6 +96,7 @@ public class PanelAddProducto extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Marco;
+    private javax.swing.JButton btnAdd;
     private javax.swing.JLabel lblCodigo;
     private javax.swing.JLabel lblNombre;
     private javax.swing.JTextField txtCodigo;
