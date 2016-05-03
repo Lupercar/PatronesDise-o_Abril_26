4aa46b355ae27333cf6576c2a7d199eba25d1c6f
JLGG <hotolete@gmail.com>
2016-05-03 22:52:20 +0200

    Ejemplo Patron Factory:Creo clase Producto con las propiedades basicas

---
+++ src/factory/Producto.java
@@ -0,0 +1,8 @@
+package factory;
+
+public class Producto {
+//    Ponemons lo que queremos que se guarde de un Producto
+    private int codigo; 
+    private String nombre; 
+    
+}//fin class Producto.factory
