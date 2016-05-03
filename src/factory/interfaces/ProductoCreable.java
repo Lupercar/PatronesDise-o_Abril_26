dac2348b8815500c84b7bbf0683213112ed3e0d0
JLGG <hotolete@gmail.com>
2016-05-03 23:39:30 +0200

    Ejemplo Patron Factory: Creo interfaz ProductoCreable y le meto los metodos abstractos

---
+++ src/factory/interfaces/ProductoCreable.java
@@ -0,0 +1,9 @@
+package factory.interfaces;
+
+public interface ProductoCreable {
+
+    int getCodigo();
+    String getNombre();
+    void setCodigo(int codigo);
+    void setNombre(String nombre);
+}//fin interface ProductoCreable.factory.interface
