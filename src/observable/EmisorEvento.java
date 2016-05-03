c4a0c97bce883a723c0a4892c30440af8254db33
JLGG <hotolete@gmail.com>
2016-05-03 10:27:05 +0200

    Ejemplo Patron Observer: Creo clase EmisorEvento, con una propiedad de tipo int llamada valor

---
+++ src/observable/EmisorEvento.java
@@ -0,0 +1,13 @@
+package observable;
+
+public class EmisorEvento {
+    private int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}//fin class EmisorEvento.observable
