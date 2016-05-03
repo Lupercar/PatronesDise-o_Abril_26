c197351687f2bc63917bee5fd4ea7b8ce75d7e65
JLGG <hotolete@gmail.com>
2016-05-03 10:38:09 +0200

    Ejemplo Patron Observer: La clase oyente implementa la interfaz OyenteListener, junto con sus metodos abstractos.

--- src/observable/Oyente.java
+++ src/observable/Oyente.java
@@ -1,9 +1,18 @@
 package observable;
+
 /*
     El "patron Observer" se utiliza para avisar a otros objetos
     que un objeto a cambiado (lanzar un evento).
     1.-Nos obliga a crear una interfaz con metodos.
-*/
-public class Oyente {
-    
+    2.-Los oyentes tienen que implementar la interfaz.
+ */
+public class Oyente implements OyenteListener {
+
+    @Override
+    public void realizarOperacion(int valor) {
+//        Metodo que se va a ejecutar cuando el emisor envie el evento
+//        indicando que ha pasado algo.
+        System.out.println("Oyente valor recibido: " + valor);
+    }
+
 }//fin class Oyente.observable
