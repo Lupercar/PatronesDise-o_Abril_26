c74937ab066d4b4332e5396aa43df350b36b9901
JLGG <hotolete@gmail.com>
2016-05-03 10:45:32 +0200

    Ejemplo Patron Observer: Clase Oyente especifica 2 caracteristicas que debe de tener los Emisores para aplicar el patron Observer.

--- src/observable/Oyente.java
+++ src/observable/Oyente.java
@@ -5,6 +5,9 @@ package observable;
     que un objeto a cambiado (lanzar un evento).
     1.-Nos obliga a crear una interfaz con metodos.
     2.-Los oyentes tienen que implementar la interfaz.
+    3.-Emisor tiene que tener metodos para add y delete un oyente al que avisar.
+    4.-Tiene que tener una lista de la interfaz donde ir almacenando los 
+    oyentes a avisar.
  */
 public class Oyente implements OyenteListener {
 
