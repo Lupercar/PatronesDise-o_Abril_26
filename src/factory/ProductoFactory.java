429b0481cc70f2a9578b8765faeda9df1a4871de
JLGG <hotolete@gmail.com>
2016-05-03 22:45:28 +0200

    Ejemplo Patron Factory:Creo clase ProductoFactory, explicacion de lo que es

---
+++ src/factory/ProductoFactory.java
@@ -0,0 +1,10 @@
+package factory;
+/*
+    El patron factory se utiliza para crear objetos en memoria.
+    A veces tenemos varias clases hijo de un padre. Con el factory 
+    con un parametro podemos elegir el objeto de la clase hijo 
+    que queremos que nos cree y devuelva.
+*/
+public class ProductoFactory {
+    
+}//fin class ProductoFactory.factory
