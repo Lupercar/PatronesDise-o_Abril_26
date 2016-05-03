cbed0948447468ae08040a89f8c42b358a7c05c3
JLGG <hotolete@gmail.com>
2016-05-03 10:28:55 +0200

    Ejemplo Patron Observer: Creo clase Oyente

---
+++ src/observable/Oyente.java
@@ -0,0 +1,9 @@
+package observable;
+/*
+    El "patron Observer" se utiliza para avisar a otros objetos
+    que un objeto a cambiado (lanzar un evento).
+    1.-Nos obliga a crear una interfaz con metodos.
+*/
+public class Oyente {
+    
+}//fin class Oyente.observable
