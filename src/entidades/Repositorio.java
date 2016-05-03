c0eaf100e95a4519348a90d5029009ab393b4810
JLGG <hotolete@gmail.com>
2016-05-03 02:45:49 +0200

    Aplico ObservableList a la clase Repositorio

--- src/entidades/Repositorio.java
+++ src/entidades/Repositorio.java
@@ -3,18 +3,22 @@ package entidades;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.jdesktop.observablecollections.ObservableCollections;
+import org.jdesktop.observablecollections.ObservableList;
 
 public class Repositorio {
     private static Repositorio instance = new Repositorio(); 
     
 //    Hago una lista de Producto
-    private List<Producto> productos = new ArrayList<>(); 
+    private List<Producto> productos = new ArrayList<>();  
+//    Creo otra lista con la ObservableList de Producto
+    private ObservableList<Producto> productosObservables = ObservableCollections.observableList(productos); 
     
     private Repositorio() {
 //      En el constructor añado 3 productos
-        productos.add(new Producto(1, "Producto 1")); 
-        productos.add(new Producto(2, "Producto 2")); 
-        productos.add(new Producto(3, "Producto 3")); 
+        productosObservables.add(new Producto(1, "Producto 1")); 
+        productosObservables.add(new Producto(2, "Producto 2")); 
+        productosObservables.add(new Producto(3, "Producto 3")); 
     }
     
     static public Repositorio getInstance(){
@@ -23,6 +27,6 @@ public class Repositorio {
     
 //    retorno la lista de productos
     public List<Producto> getProductos(){
-        return productos;
+        return productosObservables;
     }
 }//fin class Repositorio.entidades
