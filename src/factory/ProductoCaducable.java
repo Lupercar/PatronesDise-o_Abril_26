82c5dc2bbafdc5cb05d39b45c74de03e31c853c2
JLGG <hotolete@gmail.com>
2016-05-04 00:35:51 +0200

    Ejemplo Patron Factory: Le pongo codigo a la clase ProductoCaducable

--- src/factory/ProductoCaducable.java
+++ src/factory/ProductoCaducable.java
@@ -1,5 +1,14 @@
 package factory;
 
 public class ProductoCaducable extends Producto{
-    
+//    Al heredar Producto tambien implementa la interfaz ProductoCreable
+    private int diaCaducidad; 
+
+    public int getDiaCaducidad() {
+        return diaCaducidad;
+    }
+
+    public void setDiaCaducidad(int diaCaducidad) {
+        this.diaCaducidad = diaCaducidad;
+    }
 }
