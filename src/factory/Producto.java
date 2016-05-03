373d635e3f3ce0c3b0082759f2719c4faa6c45b8
JLGG <hotolete@gmail.com>
2016-05-03 23:41:09 +0200

    Ejemplo Patron Factory: En clase Producto implemento la interface ProductoCreable, le pongo codigo a sus metodos abstractos.

--- src/factory/Producto.java
+++ src/factory/Producto.java
@@ -1,9 +1,30 @@
 package factory;
 
-public abstract class Producto {
+import factory.interfaces.ProductoCreable;
+
+public abstract class Producto implements ProductoCreable {
 //    Ponemons lo que queremos que se guarde de un Producto
 //    Hago que no se pueda instanciar esta clase, la hago abstracta
     private int codigo; 
     private String nombre; 
-    
+
+    @Override
+    public int getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }//fin class Producto.factory
