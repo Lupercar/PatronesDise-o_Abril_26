db66c81e14a9000fb571655bab801b658ae8e311
JLGG <hotolete@gmail.com>
2016-05-03 12:19:29 +0200

    Ejemplo Patron Observer:Pruebo el patron en la clase App.Consola, creo un emisor y oyente, meto el oyente en la lista del emisor y le asigno un valor al emisor.

--- src/Consola/App.java
+++ src/Consola/App.java
@@ -1,5 +1,7 @@
 package Consola;
 
+import observable.EmisorEvento;
+import observable.Oyente;
 import singleton.Singleton;
 import swing.Ventana;
 
@@ -12,7 +14,17 @@ public class App {
         Singleton s = Singleton.getInstance(); //crea el objeto sino existe y lo devuelve
         
 //        Mostreamos la pantalla grafica
-        Ventana v = new Ventana(); 
-        v.setVisible(true); //con true muestra la ventana con false no.
+//        Ventana v = new Ventana(); 
+//        v.setVisible(true); //con true muestra la ventana con false no.
+
+//        Creo un emisor y un oyente
+        EmisorEvento e = new EmisorEvento(); 
+        Oyente o = new Oyente(); 
+        
+        e.EmisorEventoAdd(o); //añado un oyente al emisor
+        
+        e.setValor(100); //al emiso le cambio el valor para que lance el evento
+        //ejecuto a ver si el oyente se entera
+        
     }
 }
