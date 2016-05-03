a30fc3e64853ecfe3ad39e6eb21be9ab1de6f666
JLGG <hotolete@gmail.com>
2016-05-03 23:27:36 +0200

    Ejemplo Patron Factory: Añado en clase ProductoFactory condiciones del patron factory. Pongo la clase Producto como abstracta para que no se pueda instanciar.

--- src/factory/Producto.java
+++ src/factory/Producto.java
@@ -1,7 +1,8 @@
 package factory;
 
-public class Producto {
+public abstract class Producto {
 //    Ponemons lo que queremos que se guarde de un Producto
+//    Hago que no se pueda instanciar esta clase, la hago abstracta
     private int codigo; 
     private String nombre; 
     
--- src/factory/ProductoFactory.java
+++ src/factory/ProductoFactory.java
@@ -4,6 +4,11 @@ package factory;
     A veces tenemos varias clases hijo de un padre. Con el factory 
     con un parametro podemos elegir el objeto de la clase hijo 
     que queremos que nos cree y devuelva.
+
+    1.- Crear una interfaz que tiene que implementar las clases 
+    que se van a ver afectadas por el patron factory. En la interfaz
+    definimos la funcionalidad comun, entre las clase padre y 
+    las hijas.
 */
 public class ProductoFactory {
     
