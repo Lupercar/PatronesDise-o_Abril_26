8185c7df2fc278407f254ec333af4a2f2f7a9527
JLGG <hotolete@gmail.com>
2016-05-03 10:47:54 +0200

    Ejemplo Patron Observer: Clase EmisorEvento le añado una lista de la interfaz OyenteListener

--- src/observable/EmisorEvento.java
+++ src/observable/EmisorEvento.java
@@ -1,7 +1,11 @@
 package observable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmisorEvento {
     private int valor;
+    private List<OyenteListener> oyentes = new ArrayList<>(); 
 
     public int getValor() {
         return valor;
